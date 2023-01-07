8afcef7c34c15469259f7fa5993a870e033ab2d3
bjohnson <bretjohnson@deloitte.com>
2023-01-07 14:23:31 -0700

    assign lessons

--- src/components/Client/AssignLessons.jsx
+++ src/components/Client/AssignLessons.jsx
@@ -1,7 +1,183 @@
-import React from 'react'
+import { UserAuth } from "../../context/AuthContext";
+
+const plans = [
+  {
+    id: 1,
+    name: "Hobby",
+    memory: "4 GB RAM",
+    cpu: "4 CPUs",
+    storage: "128 GB SSD disk",
+    price: "$40",
+    isCurrent: false,
+  },
+  {
+    id: 2,
+    name: "Startup",
+    memory: "8 GB RAM",
+    cpu: "6 CPUs",
+    storage: "256 GB SSD disk",
+    price: "$80",
+    isCurrent: true,
+  },
+  // More plans...
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 export default function AssignLessons() {
+  const { students } = UserAuth();
+
+  console.log(students);
+
   return (
-    <div>AssignLessons</div>
-  )
+    <div className="max-w-7xl mx-auto">
+      <h1 className="text-2xl text-center font-bold pt-8">
+        Assign Lessons to your Students
+      </h1>
+      <h3 className="text-center text-lg">
+        Lesson's are automatically assigned when you select their Grade Level in
+        the student settings. But if you would like to assign more do so below.
+      </h3>
+      {students.map((student, planIdx) => (
+        <div
+          key={planIdx}
+          className="px-4 sm:px-6 lg:px-8 rounded-md shadow-lg border border-gray-400 mt-12 p-8"
+        >
+          <div className="sm:flex sm:items-center">
+            <div className="sm:flex-auto">
+              <h1 className="text-xl font-semibold text-gray-900">
+                {student.name}
+              </h1>
+              <p className="mt-2 text-sm text-gray-700">
+                This student on the{" "}
+                <strong className="font-semibold text-gray-900">{student.gradeLevel}</strong>{" "}
+                plan. If there are lessons you would like to assign to your student you can do so here.
+              </p>
+            </div>
+            <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+              <button
+                type="button"
+                className="inline-flex items-center justify-center rounded-md border border-transparent bg-emerald-400 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-emerald-600 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2 sm:w-auto"
+              >
+                Assign Lessons
+              </button>
+            </div>
+          </div>
+          <div className="-mx-4 mt-10 ring-1 ring-gray-300 sm:-mx-6 md:mx-0 md:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                  >
+                    Current Lessons Assigned
+                  </th>
+                  <th
+                    scope="col"
+                    className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+                  >
+                    Grade Level
+                  </th>
+                  <th
+                    scope="col"
+                    className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+                  >
+                    Status
+                  </th>
+                  <th
+                    scope="col"
+                    className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+                  >
+                    Score
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Completed Lessons
+                  </th>
+                  <th
+                    scope="col"
+                    className="relative py-3.5 pl-3 pr-4 sm:pr-6 text-right"
+                  >
+                    Fluency Videos to Grade
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td
+                    className={classNames(
+                      planIdx === 0 ? "" : "border-t border-transparent",
+                      "relative py-4 pl-4 sm:pl-6 pr-3 text-sm"
+                    )}
+                  >
+                    <div className="font-medium text-gray-900 ">
+                      {student.name}
+                    </div>
+                  </td>
+                  <td
+                    className={classNames(
+                      planIdx === 0 ? "" : "border-t border-gray-200",
+                      "hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell"
+                    )}
+                  >
+                    {student.gradeLevel}
+                  </td>
+                  <td
+                    className={classNames(
+                      planIdx === 0 ? "" : "border-t border-gray-200",
+                      "hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell"
+                    )}
+                  >
+                    <span className="inline-flex items-center rounded bg-red-100 px-2 py-0.5 text-xs font-medium text-red-800">
+                      In Progress
+                    </span>
+                  </td>
+                  <td
+                    className={classNames(
+                      planIdx === 0 ? "" : "border-t border-gray-200",
+                      "hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell"
+                    )}
+                  >
+                    No Score
+                  </td>
+                  <td
+                    className={classNames(
+                      planIdx === 0 ? "" : "border-t border-gray-200",
+                      "px-3 py-3.5 text-sm text-gray-500"
+                    )}
+                  >
+                    <a href="" className="text-emerald-500 font-bold">
+                      5
+                    </a>
+                  </td>
+                  <td
+                    className={classNames(
+                      planIdx === 0 ? "" : "border-t border-transparent",
+                      "relative py-3.5 pl-3 pr-4 sm:pr-6 text-right text-sm font-medium"
+                    )}
+                  >
+                    <button
+                      type="button"
+                      className="inline-flex items-center rounded-md border border-gray-300 bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-30"
+                      disabled={student.isCurrent}
+                    >
+                      Grade
+                    </button>
+                    {planIdx !== 0 ? (
+                      <div className="absolute right-6 left-0 -top-px h-px bg-gray-200" />
+                    ) : null}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
